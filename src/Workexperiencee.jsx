94d47328cecb3a5b017b189b15d130cce15ee8d2
sjl4365 <113270140+sjl4365@users.noreply.github.com>
2024-08-25 22:10:31 -0400

    Update Workexperiencee.jsx

--- src/Workexperiencee.jsx
+++ src/Workexperiencee.jsx
@@ -1,19 +1,80 @@
 
+// function Workexperience(){
+
+//     return(<div className="RelevantWorkExperience">
+//         <h2 >Relevant Work Experience</h2>
+//         <div >
+//             <div className="JobOne">
+//                 <h3 className="TheScore">QA Analyst @ theScore</h3>
+//                 <h4 className="DateOne">Jan 2024 - Aug 2024</h4>
+//             </div>
+//             <ul className="list-one">
+//                 <li>Working on 9 releases on wagering team and conducted regression testing, backward compatibility, migration testing,
+//                     and production smoke testing in different environments (UAT, Demo, and Staging) and devices (android, iOS, web) for
+//                     theScore Bet and ESPN (~2000 test cases)</li>
+//             <li>Created 3 new functional tickets on JIRA and Confluence by finding bugs on production environment, signed off on
+// functional tickets (~40) by creating new test cases on TestRail and writing report</li>
+//             <li>Participated in a weekly stand up by occasionally leading a session and helping other co-workers and from different
+// department need clarification on wagering</li>
+//             </ul>
+//         </div>
+//         <div >
+//             <div className="JobTwo">
+//                 <h3 className="VC">Software Developer @ Venture Canada</h3>
+//                 <h4 className="DateTwo">May 2023 – Jun 2023</h4>
+//             </div>
+//             <ul>
+//                 <li>Used PHP programming language to implement an interactive web-based platform for delivering math practice
+// problems to students</li>
+//                 <li>Designed and implemented a diverse collection of short math practice problems of 100 questions</li>
+//             </ul>
+//         </div>
+//         <div >
+//             <div className="JobThree">
+//                 <h3 className="Ruii">Software Developer @ Ruii</h3>
+//                 <h4 className="DateThree">Mar 2023 – Apr 2023</h4>
+//             </div>
+//             <ul>
+//                 <li>Implemented additional web plug-ins, enhancing customer engagement and satisfaction by using HTML and CSS</li>
+//                 <li>Optimized the website by understanding UI/UX principles, cross-browser compatibility, and adhering to general web
+// functionality and standards</li>
+//                 <li>Collaborated closely with cross-functional teams, fostering teamwork and effective communication while consistently
+// meeting project deadline</li>
+//             </ul>
+//         </div>
+//     </div>);
+// }
+// export default Workexperience
+
 function Workexperience(){
 
-    return(<div className="RelevantWorkExperience">
-        <h2 >Relevant Work Experience</h2>
+
+    return(
+    
+
+    <div className="RelevantWorkExperience">
+        <h2 className="WorkExperience" >Relevant Work Experience</h2>
+
         <div >
             <div className="JobOne">
                 <h3 className="TheScore">QA Analyst @ theScore</h3>
                 <h4 className="DateOne">Jan 2024 - Aug 2024</h4>
             </div>
-            <ul className="list-one">
+            <ul className="listOne">
+
+                <li>Worked on 17 releases on wagering team and conducted regression testing, backward compatibility, migration
+testing, and production smoke testing in different environments (UAT, Demo, and Staging) and devices (android, iOS,
+web) for theScore Bet and ESPN (~3000 test cases)</li>
+            <li>Created 5 new functional tickets on JIRA and Confluence by identifying bugs on production environment, signed off on
+~140 functional tickets (including 2 new features related to the rebranding of market logo and the recommendation of
+certain popular markets to display on the home page) by creating new test cases on TestRail and writing report</li>
+
                 <li>Working on 9 releases on wagering team and conducted regression testing, backward compatibility, migration testing,
                     and production smoke testing in different environments (UAT, Demo, and Staging) and devices (android, iOS, web) for
                     theScore Bet and ESPN (~2000 test cases)</li>
             <li>Created 3 new functional tickets on JIRA and Confluence by finding bugs on production environment, signed off on
 functional tickets (~40) by creating new test cases on TestRail and writing report</li>
+
             <li>Participated in a weekly stand up by occasionally leading a session and helping other co-workers and from different
 department need clarification on wagering</li>
             </ul>
@@ -23,7 +84,7 @@ department need clarification on wagering</li>
                 <h3 className="VC">Software Developer @ Venture Canada</h3>
                 <h4 className="DateTwo">May 2023 – Jun 2023</h4>
             </div>
-            <ul>
+            <ul className="listTwo">
                 <li>Used PHP programming language to implement an interactive web-based platform for delivering math practice
 problems to students</li>
                 <li>Designed and implemented a diverse collection of short math practice problems of 100 questions</li>
@@ -34,7 +95,7 @@ problems to students</li>
                 <h3 className="Ruii">Software Developer @ Ruii</h3>
                 <h4 className="DateThree">Mar 2023 – Apr 2023</h4>
             </div>
-            <ul>
+            <ul className="listThree">
                 <li>Implemented additional web plug-ins, enhancing customer engagement and satisfaction by using HTML and CSS</li>
                 <li>Optimized the website by understanding UI/UX principles, cross-browser compatibility, and adhering to general web
 functionality and standards</li>
