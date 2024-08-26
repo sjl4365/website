52195ebca9e3d975d0018a60e9e1f148b86f855c
sjl4365 <113270140+sjl4365@users.noreply.github.com>
2024-08-25 22:06:05 -0400

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,40 @@
+// const Navbar=()=>{
+//     return(
+//         <nav>
+            
+//             <div className="links">
+                
+//                 <a href="/" className='HomeRoute'>Home</a> 
+//                 <a href="/work" className='WorkRoute'>Work Experience </a>
+//                 <a href="/hackathon" className='HackRoute'>Hackathons/Projects</a>
+//             </div>
+            
+//         </nav>
+//     );
+// }
+// export default Navbar;
+
+
+import {Link} from 'react-router-dom'
+
+
 const Navbar=()=>{
     return(
         <nav>
-            
             <div className="links">
-                
+
+            <Link to="/" className="HomeRoute">Home</Link>
+            <Link to="/work" className="WorkRoute">Work Experience</Link>
+            <Link to="/hackathon" className='HackRoute'>Hackathon</Link>
+            <Link to="/project" className="ProjRoute">Projects</Link>
+                {/* <a href="/" className='HomeRoute'>Home</a> 
+                <a href="/work" className='WorkRoute'>Work Experience </a>
+                <a href="/hackathon" className='HackRoute'>Hackathons/Projects</a> */}
+
                 <a href="/" className='HomeRoute'>Home</a> 
                 <a href="/work" className='WorkRoute'>Work Experience </a>
                 <a href="/hackathon" className='HackRoute'>Hackathons/Projects</a>
+
             </div>
             
         </nav>
