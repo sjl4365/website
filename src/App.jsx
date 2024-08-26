e220013b4b52b68b37ff2348bf56038ea191c894
sjl4365 <113270140+sjl4365@users.noreply.github.com>
2024-08-25 22:03:20 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,69 @@
 
+// import './App.css'
+// import Home from './Home'
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Workexperience from './Workexperiencee'
+// import Navbar from './Navbar'
+// import HackathonProject from './HackathonProject'
+
+// function App() {
+
+//   return (
+    
+//     <Router>
+//       <div className='Route'>
+//         <Navbar/>
+//         <div>
+//         <Routes>
+//           <Route path="/" element={<Home/>}/>
+//           <Route path="/work" element={<Workexperience/>}/>
+//           <Route path="/hackathon" element={<HackathonProject/>}/>
+//       </Routes>
+//         </div>
+//       </div>
+//     </Router>
+//   );
+// }
+// export default App 
+
 import './App.css'
 import Home from './Home'
+
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Workexperience from './Workexperiencee';
+import Navbar from './Navbar';
+import Hackathon from './Hackathon';
+import Project from './Project';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Workexperience from './Workexperiencee'
 import Navbar from './Navbar'
 import HackathonProject from './HackathonProject'
 
+
 function App() {
 
   return (
     
+
+    // <Router basename='/MyWebsite'>
+
     <Router>
       <div className='Route'>
         <Navbar/>
-        <div>
+        
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/work" element={<Workexperience/>}/>
+
+          <Route path="/hackathon" element={<Hackathon/>}/>
+          <Route path="/project" element={<Project/>}/>
+
           <Route path="/hackathon" element={<HackathonProject/>}/>
+
       </Routes>
-        </div>
       </div>
     </Router>
   );
